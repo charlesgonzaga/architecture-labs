8c62ffce19477cdd332205dd30b483c20350fff3
Charles Gonzaga <charlesgonzag@hotmail.com>
2026-07-07 13:18:37 -0300

    feat: add customer entity

---
+++ examples/002-cqrs/src/ArchitectureLabs.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+namespace ArchitectureLabs.Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
